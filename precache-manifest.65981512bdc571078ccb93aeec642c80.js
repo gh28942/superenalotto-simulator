cfd0ae13550685deaa71ffdacce0cbcf49a012e2
gh28942 <GerH@example.com>
2021-03-05 16:36:18 +0100

    Updates

---
+++ precache-manifest.65981512bdc571078ccb93aeec642c80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b524a44aa821751d90e2ebebf12eac",
+    "url": "/gh28942/superenalotto-simulator/index.html"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/gh28942/superenalotto-simulator/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "3b87f429ff27264d7c97",
+    "url": "/gh28942/superenalotto-simulator/static/css/main.3df9e81c.chunk.css"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/gh28942/superenalotto-simulator/static/js/2.effe3d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh28942/superenalotto-simulator/static/js/2.effe3d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b87f429ff27264d7c97",
+    "url": "/gh28942/superenalotto-simulator/static/js/main.0537298e.chunk.js"
+  },
+  {
+    "revision": "3723481643f64b247c3f",
+    "url": "/gh28942/superenalotto-simulator/static/js/runtime-main.df8fa7fd.js"
+  }
+]);
